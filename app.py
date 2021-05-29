93523f1109ef522de23c200e663af0301935f52e
vijusuren <vijayalakshmioviya@gmail.com>
2021-05-29 10:10:19 +0000

    add logout functionality

--- app.py
+++ app.py
@@ -77,6 +77,14 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/logout")
+def logout():
+    # remove user from session cookies
+    flash("You have been logged out")
+    session.pop("user")
+    return redirect(url_for("login"))
+
+    
 @app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     # grab the session user's username from db
